10994d0ce1b5bf2c4532a3684a1a938a7b1fa217
katawful <katisntgood@gmail.com>
2024-06-23 06:22:22 -0400

    feat(nvim-anisole-macros): Add fennel macros from nvim-anisole

---
+++ fnl/plugins/nvim-anisole-macros.fnl
@@ -0,0 +1,4 @@
+(import-macros lazy :nvim-anisole/macros/lazy)
+
+(lazy.spec.init nvim-anisole-macros
+                (lazy.spec.repo.dir "~/Repos/NEOVIM/nvim-anisole-macros/"))
---
+++ lua/plugins/nvim-anisole-macros.lua
@@ -0,0 +1,4 @@
+-- [nfnl] Compiled from fnl/plugins/nvim-anisole-macros.fnl by https://github.com/Olical/nfnl, do not edit.
+local nvim_anisole_macros = {}
+nvim_anisole_macros["dir"] = "~/Repos/NEOVIM/nvim-anisole-macros/"
+return nvim_anisole_macros
