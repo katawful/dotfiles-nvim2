f3a0ba91bd4a05ec63ac564b740be3c4f829dd4f
katawful <katisntgood@gmail.com>
2024-06-23 06:24:12 -0400

    feat(plenary): Add plenary nvim runtime library

---
+++ fnl/plugins/plenary.fnl
@@ -0,0 +1,4 @@
+(import-macros lazy :nvim-anisole.macros.lazy)
+
+(lazy.spec.init plenary
+                (lazy.spec.repo.gh "nvim-lua/plenary.nvim"))
---
+++ lua/plugins/plenary.lua
@@ -0,0 +1,4 @@
+-- [nfnl] Compiled from fnl/plugins/plenary.fnl by https://github.com/Olical/nfnl, do not edit.
+local plenary = {}
+plenary[1] = "nvim-lua/plenary.nvim"
+return plenary
