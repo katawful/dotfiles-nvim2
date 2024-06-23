c24ed540ab643c833496fd53ec44585e30313d9e
katawful <katisntgood@gmail.com>
2024-06-23 06:14:39 -0400

    feat(katdotnvim): Add colorscheme

---
+++ fnl/plugins/katdotnvim.fnl
@@ -0,0 +1,4 @@
+{:enabled true
+ :priority 1000
+ :dir "~/Repos/NEOVIM/katdotnvim/"
+ :config #(vim.cmd.colorscheme :kat.nvim)}
---
+++ lua/plugins/katdotnvim.lua
@@ -0,0 +1,5 @@
+-- [nfnl] Compiled from fnl/plugins/katdotnvim.fnl by https://github.com/Olical/nfnl, do not edit.
+local function _1_()
+  return vim.cmd.colorscheme("kat.nvim")
+end
+return {enabled = true, priority = 1000, dir = "~/Repos/NEOVIM/katdotnvim/", config = _1_}
