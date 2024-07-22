33dfbce2c632c27952160f61d17c0086ef66827e
katawful <katisntgood@gmail.com>
2024-07-22 06:18:49 -0400

    feat(zen-mode): Add keybinding to toggle

--- fnl/plugins/zenmode.fnl
+++ fnl/plugins/zenmode.fnl
@@ -1,1 +1,6 @@
-{1 :folke/zen-mode.nvim}
+(local leader :<leader>z)
+
+{1 :folke/zen-mode.nvim
+ :keys [{1 (.. leader :t)
+         2 #((. (require :zen-mode) :toggle))
+         :desc "ZenMode -- Toggle zen-mode"}]}
--- lua/plugins/zenmode.lua
+++ lua/plugins/zenmode.lua
@@ -1,2 +1,6 @@
 -- [nfnl] Compiled from fnl/plugins/zenmode.fnl by https://github.com/Olical/nfnl, do not edit.
-return {"folke/zen-mode.nvim"}
+local leader = "<leader>z"
+local function _1_()
+  return (require("zen-mode")).toggle()
+end
+return {"folke/zen-mode.nvim", keys = {{(leader .. "t"), _1_, desc = "ZenMode -- Toggle zen-mode"}}}
