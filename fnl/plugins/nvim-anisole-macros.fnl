b35557743d6b77865c6d7ea43544ee7506597ba4
Kat <katisntgood@gmail.com>
2025-04-08 22:29:21 -0400

    fix(anisole-macros): Fix multi-device errors

--- fnl/plugins/nvim-anisole-macros.fnl
+++ fnl/plugins/nvim-anisole-macros.fnl
@@ -1,1 +1,11 @@
-{:dir "~/Repos/NEOVIM/nvim-anisole-macros/"}
+;;; Autoload -- improves performance for this plugin
+(local {: autoload} (require :nfnl.module))
+(local sys (autoload :globals.init))
+
+(var M {})
+
+(if (= sys.home :Kat-Arch)
+    (set M {:dir "~/Repos/NEOVIM/nvim-anisole-macros/"})
+    (set M [:katawful/nvim-anisole-macros/]))
+
+M
--- lua/plugins/nvim-anisole-macros.lua
+++ lua/plugins/nvim-anisole-macros.lua
@@ -1,2 +1,11 @@
 -- [nfnl] Compiled from fnl/plugins/nvim-anisole-macros.fnl by https://github.com/Olical/nfnl, do not edit.
-return {dir = "~/Repos/NEOVIM/nvim-anisole-macros/"}
+local _local_1_ = require("nfnl.module")
+local autoload = _local_1_["autoload"]
+local sys = autoload("globals.init")
+local M = {}
+if (sys.home == "Kat-Arch") then
+  M = {dir = "~/Repos/NEOVIM/nvim-anisole-macros/"}
+else
+  M = {"katawful/nvim-anisole-macros/"}
+end
+return M
